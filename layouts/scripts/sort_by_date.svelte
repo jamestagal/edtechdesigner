371b1f61e7f0f210c4ac9d9495522b26691d2319
jimafisk <jimafisk@gmail.com>
2022-01-04 16:25:32 -0500

    Order posts by date (#13).

---
+++ layouts/scripts/sort_by_date.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  export const sortByDate = (items, order) => {
+    items.sort((a, b) => { 
+      // Must have field named "dateCreated" in content source to work.
+      let dateA = new Date(a?.fields?.dateCreated);
+      let dateB = new Date(b?.fields?.dateCreated);
+      return (order == "oldest") ? (dateA - dateB) : (dateB - dateA);
+    });
+    return items;
+  };
+</script>
