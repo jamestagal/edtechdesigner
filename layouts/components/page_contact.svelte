62ff050bfffd43e85826af5ad78d37d607750475
jamestagal <benjaminjameswaller@gmail.com>
2022-01-22 10:56:11 +1100

    add back data-netlify attribute

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -65,7 +65,7 @@
             </h2>
           <p class="mb-10">{articleBody}</p>
 
-        <form method="POST" name="contactform" class="w-full max-w-lg">
+        <form method="POST" name="contactform" data-netlify="true" class="w-full max-w-lg">
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
