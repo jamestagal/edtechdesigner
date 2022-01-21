c2fbcdbc4821c5138bdcf038706eaf74b0803240
jamestagal <benjaminjameswaller@gmail.com>
2022-01-21 14:32:03 +1100

    change 'action' attribute to 'method' in form

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -65,7 +65,7 @@
             </h2>
           <p class="mb-10">{articleBody}</p>
 
-        <form action="POST" data-netlify="true" name="contact form" class="w-full max-w-lg">
+        <form method="POST" data-netlify="true" name="contact form" class="w-full max-w-lg">
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
