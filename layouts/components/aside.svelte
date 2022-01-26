b1417747483661119928183e405b69d772bcd75b
jamestagal <benjaminjameswaller@gmail.com>
2022-01-26 13:06:13 +1100

    update from theme & style cats and tags

--- layouts/components/aside.svelte
+++ layouts/components/aside.svelte
@@ -1,9 +1,8 @@
 <script>
-  import { onMount } from "svelte";
   import Search from "../components/search.svelte";
   import Social from "../components/social.svelte";
 
-  export let allPosts, socialLinks, tagsList, catgList;
+  export let allPosts, socialLinks, catgPosts, tagsPosts;
 </script>
 
 <aside class="w-full">
@@ -18,11 +17,11 @@
     <div class="row">
       <h4 class="header mb-1"><span>Categories</span></h4>
       <ul class="text-aside mb-4">
-        {#each catgList as catg, i}
+        {#each Object(catgPosts) as {page, name, length}}
           <li class="flex rounded items-center mt-3 mr-3">
-            <div style="padding: 0px 0.5em; border: 1px solid black; border-radius: 0.25em 0px 0px 0.25em;">
-            <a href="catgs/{i + 1}">{catg}</a></div>
-            <div style="display: flex; align-items: center; font-size: 0.8em; background-color: black; color: white; border-radius: 0px 0.25em 0.25em 0px; padding: 0px 0.5em; align-self: stretch;">{catgList.length}</div>
+            <div class="border border-y-black border-l-black" style="padding: 0px 0.5em; border-radius: 0.25em 0px 0px 0.25em;">
+            <a href="catgs/{page}">{name}</a></div>
+            <div style="display: flex; align-items: center; font-size: 0.8em; background-color: var(--surface); color: white; border-radius: 0px 0.25em 0.25em 0px; padding: 0px 0.5em; align-self: stretch;">{length}</div>
           </li>
         {/each}
       </ul>
@@ -32,9 +31,11 @@
     <div class="row">
       <h4 class="header mb-1 w-full flex">Tags</h4>
       <ul class="text-aside mb-4">
-        {#each tagsList as tag, i}
-          <li class="flex">
-            <a href="tags/{i + 1}">{tag}</a>
+        {#each Object(tagsPosts) as {page, name, length}}
+          <li class="flex rounded items-center mt-3 mr-3">
+            <div class="border border-y-black border-l-black" style="padding: 0px 0.5em; border-radius: 0.25em 0px 0px 0.25em;">
+            <a href="tags/{page}">{name}</a></div>
+            <div style="display: flex; align-items: center; font-size: 0.8em; background-color: var(--surface); color: white; border-radius: 0px 0.25em 0.25em 0px; padding: 0px 0.5em; align-self: stretch;">{length}</div>
           </li>
         {/each}
       </ul>
