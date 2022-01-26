937a4ad1238c87278cf1b1fd6df51b310c1a9053
jamestagal <benjaminjameswaller@gmail.com>
2022-01-26 13:11:14 +1100

    updates from theme commits Jan 22, 2022

--- layouts/scripts/sort_by_date.svelte
+++ layouts/scripts/sort_by_date.svelte
@@ -1,11 +1,26 @@
 <script context="module">
-  export const sortByDate = (items, order) => {
-    items.sort((a, b) => { 
-      // Must have field named "dateCreated" in content source to work.
-      let dateA = new Date(a?.fields?.dateCreated.split('/').reverse().join());
-      let dateB = new Date(b?.fields?.dateCreated.split('/').reverse().join());
-      return (order == "oldest") ? (dateA - dateB) : (dateB - dateA);
+  export const sortByDate = (items, sortDate, order) => {
+    // Default sort posts created by newest first
+    var sortDate = typeof sortDate != "undefined" ? order : "created";
+    var order = typeof order != "undefined" ? order : "newest";
+
+    items.sort((a, b) => {
+      if (sortDate == "modified") {
+        // Use modified date for sort
+        let dateA = new Date(a?.fields?.dateModified.split('/').reverse().join());
+        let dateB = new Date(b?.fields?.dateModified.split('/').reverse().join());
+
+        return order == "oldest" ? dateA - dateB : dateB - dateA;
+      } else {
+        // Use create date for sort
+        let dateA = new Date(a?.fields?.dateCreated.split('/').reverse().join());
+        let dateB = new Date(b?.fields?.dateCreated.split('/').reverse().join());
+
+        return order == "oldest" ? dateA - dateB : dateB - dateA;
+      }
     });
+
+    // return the sorted object
     return items;
   };
 </script>
