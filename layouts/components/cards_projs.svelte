3ba1594e8fb2f3408b4e4195dbc14bee46310adb
jamestagal <benjaminjameswaller@gmail.com>
2022-01-26 13:08:51 +1100

    updates from theme commits Jan 22, 2022

--- layouts/components/cards_projs.svelte
+++ layouts/components/cards_projs.svelte
@@ -1,54 +1,60 @@
 <script>
   import PostMeta from "./post_meta.svelte";
-  import { sortByDate } from '../scripts/sort_by_date.svelte';
-  export let 
-    uniqProjs,
-    allProjs,
-    projRangeHigh,
-    projRangeLow,
-    tagsList,
-    catgList;
+  export let projList, projRangeHigh, projRangeLow, catgPosts, tagsPosts;
   export let complete = true;
   export let skipbody = true;
+  let projTLDR = true;
 </script>
+
 <h2 class="header text-xl md:text-2xl lg:text-3xl">
   <span class="accent">Posts</span>
 </h2>
-{#each uniqProjs as proj, i}
+{#each projList as proj, i}
   {#if i >= projRangeLow && i < projRangeHigh}
     <div class="rounded-xl overflow-hidden shadow-md bg-secondary">
       <div class="flex-none lg:flex">
         <div class="mx-2 md:mx-6 mb-2 md:mb-3 my-1">
-          <h2 class="header text-xl md:text-2xl lg:text-3xl my-5">
-            {proj}
-          </h2>
-          {#each sortByDate(allProjs) as post, p}
-            {#if proj == post.fields.posts}
-              <div class="flex items-center mb-3">
-                <img
-                  class="inline-block object-cover rounded-md w-28 h-28"
-                  src="assets/posts/{post.fields.image.src}"
-                  alt={post.fields.image.alt}
-                />
-                <div class="inline-block ml-2">
-                  <h3 class="header mt-0 mb-1 text-xl md:text-2xl">
-                    <a href={post.path}>{post.fields.title}</a>
-                  </h3>
-                  <ul class="text-meta flex flex-wrap">
-                    <li class="mx-0 -mt-1 text-meta inline-flex">
-                      <i class="las la-user-astronaut text-lg" />
-                    </li>
-                    <PostMeta
-                      {post}
-                      {tagsList}
-                      {catgList}
-                      {complete}
-                      {skipbody}
-                    />
-                  </ul>
-                </div>
-              </div>
+          {#each proj.posts as post, p}
+            <!-- print the project heading only once -->
+            {#if p == 0}
+              <h2 class="header text-xl md:text-2xl lg:text-3xl my-5">
+                {proj.name}
+              </h2>
+              <!-- print the TL;DR for the heading post -->
+              {#if projTLDR}
+                <p class="text-base mb-5">
+                  {post.fields.articleBody.substring(
+                    post.fields.articleBody.indexOf("<p>") + 3,
+                    post.fields.articleBody.indexOf("<h2")
+                  )}
+                </p>
+              {/if}
             {/if}
+            <!-- print the project card -->
+            <div class="flex items-center mb-3">
+              <img
+                class="inline-block object-cover rounded-md w-28 h-28"
+                src="assets/posts/{post.fields.image.src}"
+                alt={post.fields.image.alt}
+              />
+              <div class="inline-block ml-2">
+                <h3 class="header mt-0 mb-1 text-xl md:text-2xl">
+                  <a href={post.path}>{post.fields.title}</a>
+                </h3>
+                <ul class="text-meta flex flex-wrap">
+                  <li class="mx-0 -mt-1 text-meta inline-flex">
+                    <i class="las la-user-astronaut text-lg" />
+                  </li>
+                  <PostMeta
+                    {post}
+                    {catgPosts}
+                    {tagsPosts}
+                    {complete}
+                    {skipbody}
+                  />
+                </ul>
+              </div>
+            </div>
           {/each}
         </div>
       </div>
