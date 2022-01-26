637e0be64c8c17fb510f5eadde2ee8370fa6875c
jamestagal <benjaminjameswaller@gmail.com>
2022-01-26 13:09:58 +1100

    updates from theme commits Jan 22, 2022

--- layouts/content/posts.svelte
+++ layouts/content/posts.svelte
@@ -4,7 +4,7 @@
   import PostMeta from "../components/post_meta.svelte";
 
   // Values passed in from "html.svelte"
-  export let idxContent, allPosts, tagsList, catgList;
+  export let idxContent, allPosts, catgPosts, tagsPosts;
 
   // Content field Values passed in via layout content {...content.fields}
   export let articleBody, title, image, author;
@@ -42,7 +42,7 @@
           <li class="mx-0 -mt-1 text-meta inline-flex">
           <i class="las la-user-astronaut text-lg" />
         </li>
-        <PostMeta {post} {tagsList} {catgList} {complete} {skipbody} />
+        <PostMeta {post} {catgPosts} {tagsPosts} {complete} {skipbody} />
       </ul>
       <p class="mt-6">
           {@html articleBody}
@@ -50,7 +50,7 @@
       </div>
         <!-- Aside  -->
         <div class="w-full order-last md:order-none md:w-3/12 mb-5 mb-lg-0 px-0">
-          <Aside {allPosts} {socialLinks} {tagsList} {catgList} />
+          <Aside {allPosts} {catgPosts} {tagsPosts} {socialLinks} />
         </div>
       <div class="relative mb-8 w-full">
         {#if h5p}
