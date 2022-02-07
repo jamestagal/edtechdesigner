9cc83278465f23bb2a3b1ffb366b7429acf3a247
jamestagal <benjaminjameswaller@gmail.com>
2022-02-07 20:53:36 +1100

    Add active class to aside for tags and catgs

--- layouts/components/aside.svelte
+++ layouts/components/aside.svelte
@@ -2,7 +2,7 @@
   import Search from "../components/search.svelte";
   import Social from "../components/social.svelte";
 
-  export let allPosts, socialLinks, catgPosts, tagsPosts;
+  export let catg, tag, allPosts, socialLinks, catgPosts, tagsPosts;
   
   let current = false;
  
@@ -22,7 +22,7 @@
       <h4 class="header mb-1"><span>Categories</span></h4>
       <ul class="text-aside mb-4">
         {#each Object(catgPosts) as {page, name, length}}
-          <li class="flex rounded items-center mt-3 mr-3">
+          <li class="flex{catg == name ? ' active' : ''} rounded items-center mt-3 mr-3">
             <div class="customStyle border border-y-black border-l-black" class:selected={current}>
             <a on:click={() => current = true} href="catgs/{page}">{name}</a></div>
             <div style="display: flex; align-items: center; font-size: 0.8em; background-color: var(--surface); color: white; border-radius: 0px 0.25em 0.25em 0px; padding: 0px 0.5em; align-self: stretch;">{length}</div>
@@ -36,7 +36,7 @@
       <h4 class="header mb-1 w-full flex">Tags</h4>
       <ul class="text-aside mb-4">
         {#each Object(tagsPosts) as {page, name, length}}
-          <li class="flex rounded items-center mt-3 mr-3">
+          <li class="flex{tag == name ? ' active' : ''} rounded items-center mt-3 mr-3">
             <div class="customStyle border border-inherit border-y-black border-l-black">
             <a on:click href="tags/{page}">{name}</a></div>
             <div style="display: flex; align-items: center; font-size: 0.8em; background-color: var(--surface); color: white; border-radius: 0px 0.25em 0.25em 0px; padding: 0px 0.5em; align-self: stretch;">{length}</div>
@@ -49,6 +49,9 @@
 <style>
   a:active {
     color: #F1F5F9;
+  }
+  .active {
+    color: var(--accent);
   }
   .customStyle {
     padding: 0px 0.5em;
