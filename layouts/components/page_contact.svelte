84b76e590cde38de4f707e9fd71015200569b49d
jamestagal <benjaminjameswaller@gmail.com>
2022-05-05 23:20:09 +1000

    update netlify form attribute

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -51,11 +51,12 @@
             <p class="mb-10">{articleBody}</p>
             <form
               data-netlify="true"
-              name="contact"
+              id="contact"
               method="POST"
               action={reqUrl}
               class="w-full max-w-lg"
               on:submit|preventDefault={handleOnSubmit}
+              <input type="hidden" name="form-name" value="contact" />
             >
               <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
