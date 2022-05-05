bfb90241f53993895950152d360028373d4e71d8
jamestagal <benjaminjameswaller@gmail.com>
2022-05-05 23:22:20 +1000

    change code line for input field

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -56,8 +56,8 @@
               action={reqUrl}
               class="w-full max-w-lg"
               on:submit|preventDefault={handleOnSubmit}
-              <input type="hidden" name="form-name" value="contact" />
             >
+            <input type="hidden" name="form-name" value="contact" />
               <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                   <label
