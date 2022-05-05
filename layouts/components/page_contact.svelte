4c5f3294aa2d59f5539a9f185fdffa761852b2b6
jamestagal <benjaminjameswaller@gmail.com>
2022-05-05 23:50:54 +1000

    add more Netlify form attributes

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -51,12 +51,15 @@
             <p class="mb-10">{articleBody}</p>
             <form
               data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              data-netlify-recaptcha="true"
               name="contact"
               method="POST"
               action={reqUrl}
               class="w-full max-w-lg"
               on:submit|preventDefault={handleOnSubmit}
             >
+            <input class="hidden" name="bot-field">
               <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                   <label
