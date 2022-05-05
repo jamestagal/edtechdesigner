213a7d396d2a14df8afc0aa272a49bd39a7e7ea8
jamestagal <benjaminjameswaller@gmail.com>
2022-05-05 23:30:06 +1000

    remove input field

--- layouts/components/page_contact.svelte
+++ layouts/components/page_contact.svelte
@@ -57,7 +57,6 @@
               class="w-full max-w-lg"
               on:submit|preventDefault={handleOnSubmit}
             >
-            <input type="hidden" name="form-name" value="contact" />
               <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                   <label
