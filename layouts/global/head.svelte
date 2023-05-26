8641c51b6c98f033e844d9f367e4775e6735f404
jamestagal <benjaminjameswaller@gmail.com>
2023-05-26 23:18:43 +1000

    add Google tag  gtag.js

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -10,6 +10,15 @@
 </script>
 
 <head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HZ73YNNJM2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HZ73YNNJM2');
+</script>
   <meta charset="utf-8" />
   <meta name="google-site-verification" content="yCeUyDaIyXBCoAE1818fhgmRan4SH3G1DDtidwXjm1Q" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
